2e41ffe4ec798cfa3824fe4cbe51558c0490b616
gelevent <gemiwidodosmktibazma@gmail.com>
2025-02-24 11:10:32 +0700

    gitignore

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
         });
         console.log(`MongoDB Terkoneksi: ${conn.connection.host}`);
     } catch (error) {
